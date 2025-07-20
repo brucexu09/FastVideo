40bf96c7361599d8ad386e1b3701e91401968b12
brucexu <boxunxu@meta.com>
2025-07-19 19:10:58 -0700

    add bwd scripts

---
+++ csrc/attn/benchmarks/benchmark_trace_and_latency/bench_aparse_atten_bwd.sh
@@ -0,0 +1,3 @@
+python bench_bwd.py --bench_atten_type "FA3" --test_latency --torch_compile
+python bench_bwd.py --bench_atten_type "VSA" --test_latency --torch_compile
+python bench_bwd.py --bench_atten_type "VSA_flex" --test_latency --torch_compile
